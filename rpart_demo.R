1f43bc7cf4ebeecdc27c33c3cff576d53520c968
cherryBomba58 <repassy.adrienn@gmail.com>
2017-12-04 03:29:40 +0100

    Klasztert is betettem, R-ben működik vele a tanítás, Pythonban (még) nem

--- rpart_demo.R
+++ rpart_demo.R
@@ -3,10 +3,16 @@ library("corrgram")
 
 data<-read.csv("E:/BME-MSc-2.felev/bigdata/Dropbox/data/globalterrorismdb_0617dist.csv", header = T, sep = ";", skipNul = T)
 
+# korrelációs diagram
 vars2 <- c("iyear","imonth","iday","extended","country","region", "specificity","vicinity","multiple","success","suicide", 
            "attacktype1", "targtype1", "natlty1", "guncertain1", "individual", "weaptype1", "nwound")
 round(cor(data[,vars2], use="pair"),2)
 corrgram(data[,vars2], order = T, lower.panel = panel.shade, upper.panel = panel.pie)
+
+# klaszterezés
+result<-kmeans(data[!is.na(data[,'nkill']),'nkill'],5)
+nkill_classes<-cbind(data[!is.na(data[,'nkill']),'nkill'],result$cluster)
+result$cluster
 
 # Itt adjuk hozzá az osztályozást az adatokhoz
 borders <- as.matrix(c(1,5,10,100))
@@ -29,18 +35,24 @@ for (i in 1:nrow(data))
       classes[i]<-5
   }
 }
+# az adatokat és a hozzájuk tartozó címkéket egy táblázatba tesszük
 newdata<-cbind(data,classes)
+# kiszûrjük azokat a sorokat, ahol nincs osztályozás
 newdata<-newdata[newdata[,'classes']!=-1,]
 
+# itt kezdjük el a döntési fa tanítását
 tr_idx<-sample(nrow(newdata), nrow(newdata)*0.8)
 train<-newdata[tr_idx,]
 test<-newdata[-tr_idx,]
 
+# itt lehet játszani a paraméterekkel: a fa tanítása
 tree<-rpart(factor(classes)~eventid+iyear+imonth+iday+extended+country+region+specificity+vicinity+crit1+crit2+crit3+doubtterr+
             multiple+success+suicide+attacktype1+targtype1+natlty1+guncertain1+individual+weaptype1+property+ishostkid+ransom+
             INT_LOG+INT_IDEO+INT_MISC+INT_ANY+targsubtype1+weapsubtype1+nwound+propextent+alternative+attacktype2+attacktype3+
             targtype2+targsubtype2+natlty2+targtype3+targsubtype3+natlty3+guncertain2+guncertain3+nperps+claimed+claimmode+claim2+
             claimmode2+claim3+claimmode3+compclaim+weaptype2+weapsubtype2+weaptype3+weapsubtype3+weaptype4+weapsubtype4+nwoundte+
             nhostkid+hostkidoutcome+ndays+nreleased,data=train)
+# kirajzolja a fát
 plot(tree)
+# szöveggel látja el az ábrát
 text(tree,cex=.8)
